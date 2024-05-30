38ed08d6438fc9668b50e9742047b05452146d40
Yancap <yangabriel3001f@gmail.com>
2024-05-30 20:39:39 -0300

    prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jogo-angular';
+
+  public modal = false;
+
+
+  ngOnInit(): void {
+    window.addEventListener('keydown', (event: KeyboardEvent) => {
+      console.log(event);
+      if(event.key === 'Escape') {
+        this.modal = false;
+      }
+
+    })
+  }
+  public openModal() {
+    this.modal = true;
+  };
+  public closeModal(event?: MouseEvent) {
+    const bgModal = document.querySelector('.modal-bg')
+    console.log(event?.currentTarget);
+
+    if(event?.currentTarget === bgModal) {
+      this.modal = false;
+    }
+  };
 }
